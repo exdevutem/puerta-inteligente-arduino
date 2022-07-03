6440e8bc7ef89929b1441933508c933389fd4032
PollitoMayo <jp.vergaran@gmail.com>
2022-07-03 18:29:45 -0400

    RFID initial logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <MFRC522.h>
+#include <SPI.h>
 #include <Wire.h>
 
 #include "../lib/Display/src/Display.h"
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);         // Set for Display. Uses pin A4, A5
-#define button1Pin 2                        // Set for Button 1. Uses pin 2.
-#define button2Pin 3                        // Set for Button 2. Uses pin 3.
-#define button3Pin 4                        // Set for Button 3. Uses pin 4.
-#define button4Pin 5                        // Set for Button 4. Uses pin 5.
-#define innerButtonPin 6                    // Set for inner Button. Uses pin 6.
+#define BUTTON1PIN 2                        // Set for Button 1. Uses pin 2.
+#define BUTTON2PIN 3                        // Set for Button 2. Uses pin 3.
+#define BUTTON3PIN 4                        // Set for Button 3. Uses pin 4.
+#define BUTTON4PIN 5                        // Set for Button 4. Uses pin 5.
+#define INNERBUTTONPIN 6                    // Set for inner Button. Uses pin 6.
 int password[4];                            // Array for password.
 const int dummyPassword[4] = {1, 2, 3, 4};  // Array with an example password.
+#define RFIDRESETPIN 7                      // Set for RFID. Uses pin 7.
+#define RFIDSSPIN 8                         // Set for RFID. Uses pin 8.
+#define RFIDMOSIPIN 9                       // Set for RFID. Uses pin 9.
+#define RFIDMISOPIN 10                      // Set for RFID. Uses pin 10.
+#define RFIDSCKPIN 11                       // Set for RFID. Uses pin 11.
+MFRC522 rfid(RFIDSSPIN, RFIDRESETPIN);      // Set for RFID.
+byte nuidPICC[4];                           // Array to store UUID of the NFC
+bool passwordProcess = false;               // Password process status
 
 #define I2C_BUS_ADDRESS 8
 
@@ -21,7 +31,7 @@ void startDisplay() {
     lcd.init();
     lcd.backlight();
     lcd.clear();
-    lcd.print("Display iniciado");
+    lcd.print("Club ExDev");
 }
 // Writer
 void writeDisplay(String firstLine, String secondLine) {
@@ -31,13 +41,13 @@ void writeDisplay(String firstLine, String secondLine) {
     lcd.print(secondLine);
 }
 
-// Buttons functions
+// Buttons and Password functions
 void startButtons() {
-    pinMode(button1Pin, INPUT);
-    pinMode(button2Pin, INPUT);
-    pinMode(button3Pin, INPUT);
-    pinMode(button4Pin, INPUT);
-    pinMode(innerButtonPin, INPUT);
+    pinMode(BUTTON1PIN, INPUT);
+    pinMode(BUTTON2PIN, INPUT);
+    pinMode(BUTTON3PIN, INPUT);
+    pinMode(BUTTON4PIN, INPUT);
+    pinMode(INNERBUTTONPIN, INPUT);
 }
 bool passwordIsFillable() {
     bool fillable = false;
@@ -73,36 +83,78 @@ bool comparePasswords() {
     return true;
 }
 void checkButtons() {
-    int button1State = digitalRead(button1Pin);
-    int button2State = digitalRead(button2Pin);
-    int button3State = digitalRead(button3Pin);
-    int button4State = digitalRead(button4Pin);
-    int innerButtonState = digitalRead(innerButtonPin);
+    int button1State = digitalRead(BUTTON1PIN);
+    int button2State = digitalRead(BUTTON2PIN);
+    int button3State = digitalRead(BUTTON3PIN);
+    int button4State = digitalRead(BUTTON4PIN);
+    int innerButtonState = digitalRead(INNERBUTTONPIN);
 
-    if (button1State == HIGH || button2State == HIGH || button3State == HIGH ||
-        button4State == HIGH) {
-        if (passwordIsFillable()) {
-            // When the password array has at least one 0.
-            if (button1State == HIGH) {
-                setPasswordValue(1);
-            } else if (button2State == HIGH) {
-                setPasswordValue(2);
-            } else if (button3State == HIGH) {
-                setPasswordValue(3);
-            } else if (button4State == HIGH) {
-                setPasswordValue(4);
-            }
+    if (innerButtonState == HIGH) {
+        // TO-DO activate Servo
+    }
+    if (passwordProcess == true && passwordIsFillable() == true &&
+        (button1State == HIGH || button2State == HIGH || button3State == HIGH ||
+         button4State == HIGH)) {
+        // When the password array has at least one 0.
+        if (button1State == HIGH) {
+            setPasswordValue(1);
+        } else if (button2State == HIGH) {
+            setPasswordValue(2);
+        } else if (button3State == HIGH) {
+            setPasswordValue(3);
+        } else if (button4State == HIGH) {
+            setPasswordValue(4);
+        }
 
-            if (passwordIsFillable() == false) {
-                if (comparePasswords() == true) {
-                    // TO-DO: Turn on the Servo
-                    writeDisplay("Ingresar contraseña:", "Exitoso");
-                } else {
-                    // If the comparison is wrong, this won't let in.
-                    writeDisplay("Ingresar contraseña:", "Incorrecta");
-                    cleanPassword();
-                }
+        if (passwordIsFillable() == false) {
+            if (comparePasswords() == true) {
+                // TO-DO: Turn on the Servo
+                writeDisplay("Ingrese clave:", "Clave correcta");
+            } else {
+                // If the comparison is wrong, this won't let in.
+                writeDisplay("Ingrese clave:", "Clave incorrecta");
+                cleanPassword();
             }
+            passwordProcess = false;
+            delay(2000);
+            writeDisplay("Club ExDev", "");
+        }
+    }
+}
+
+// RFID functions
+void startRFID() {
+    SPI.begin();      // Init SPI bus
+    rfid.PCD_Init();  // Init MFRC522
+}
+bool compareRFIDuuids() {
+    if (rfid.uid.uidByte[0] == nuidPICC[0] &&
+        rfid.uid.uidByte[1] == nuidPICC[1] &&
+        rfid.uid.uidByte[2] == nuidPICC[2] &&
+        rfid.uid.uidByte[3] == nuidPICC[3]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+void checkRFID() {
+    // Verify if the NUID has been readed
+    if (!rfid.PICC_ReadCardSerial()) {
+        return;
+    };
+
+    if (compareRFIDuuids()) {
+        // If the uuid are equals, start the password process.
+        writeDisplay("Club ExDev", "Tarjeta reconocida");
+        passwordProcess = true;
+        cleanPassword();
+        delay(2000);
+        writeDisplay("Ingrese clave:", "");
+    } else {
+        // This is just for test. Should delete later.
+        // This store the card uuid.
+        for (byte i = 0; i < 4; i++) {
+            nuidPICC[i] = rfid.uid.uidByte[i];
         }
     }
 }
@@ -139,6 +191,7 @@ void setup() {
     Wire.onReceive(receiveEvent); /* register receive event */
     Wire.onRequest(requestEvent); /* register request event */
     Serial.begin(9600);           /* start serial for debug */
+    startRFID();
     startDisplay();
     startButtons();
 }
@@ -147,7 +200,9 @@ void loop() {
     int seconds = millis() / 1000;
     String text = String(seconds) + " Segundos";
     Serial.println(seconds);
-    writeDisplay("Prueba", text);
+
+    checkRFID();
     checkButtons();
+
     delay(1000);
 }
